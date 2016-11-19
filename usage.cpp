918755d77ad32ae24ed2258f93997973ba76e8d9
Fredrick Vamstad <fredrick.vamstad@gmail.com>
2016-11-19 23:40:36 +0100

    Initial example

---
+++ usage.cpp
@@ -0,0 +1,22 @@
+void LoadToGLTexture()
+{
+    /*
+       m_hg.generate(HeightGenerator::GenSeed(), 1024, 0.36f, 14, 0.00055f, "", "height_sample.png");
+    
+    */
+    
+    width = m_hg.generatedParam().resolution;
+    height = m_hg.generatedParam().resolution;
+
+    glGenTextures(1, &m_heightMap);
+    glBindTexture(GL_TEXTURE_2D, m_heightMap);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, width, height, 0,
+        GL_LUMINANCE, GL_UNSIGNED_SHORT, m_hg.generatedData());
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+}
